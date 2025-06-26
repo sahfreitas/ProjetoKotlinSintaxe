2998c394240d1e55baeb5f6da52292fdbbdda400
Sara da Cruz Freitas <159440011+sahfreitas@users.noreply.github.com>
2025-06-26 18:41:12 -0300

    Delete ListaES02.kt

--- ListaES02.kt
+++
@@ -1,6 +0,0 @@
-//2. Programa que peça um número e então mostre a mensagem O número informado foi [número].//
-fun main(args: Array<String>) {
-println("Digite um número:")
-val num = readLine()!!.toInt()
-println("O número informado foi $num")
-}

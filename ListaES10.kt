e54095eb1603288e2059fe96cd164409efe18130
Sara da Cruz Freitas <159440011+sahfreitas@users.noreply.github.com>
2025-06-26 18:42:20 -0300

    Delete ListaES10.kt

--- ListaES10.kt
+++
@@ -1,8 +0,0 @@
-//10. Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.//
-fun main(args: Array<String>) {
-println("Digite a temperatura em graus Celcius:")
-val c = readLine()!!.toDouble()
-println("Digite as horas trabalhadas:")
-val f = c * 1.8 + 32
-println("A temperatura em Fahrenheit é: $f")
-}

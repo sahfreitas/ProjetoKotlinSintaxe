319f1b5d9b6855daaeec6df6c185c9e53c0e35ac
Sara da Cruz Freitas <159440011+sahfreitas@users.noreply.github.com>
2025-06-26 18:41:40 -0300

    Delete ListaES05.kt

--- ListaES05.kt
+++
@@ -1,7 +0,0 @@
-//5. Programa que converta metros para centímetros.//
-fun main(args: Array<String>) {
-println("Digite o valor em metros para converter em centimetros:")
-val m = readLine()!!.toFloat()
-val cm = m * 100
-println("O valor convertido é: $cm")
-}
